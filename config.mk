0563adaf9070710ce622a48f831a80cc3721bf7c
Alexander Wirt <formorer@debian.org>
2012-09-09 10:15:47 +0200

    Imported Upstream version 2.0.2

--- config.mk
+++ config.mk
@@ -1,15 +1,5 @@
 PERL = /usr/bin/perl
 
 PREFIX = /usr
-MANDIR = $(PREFIX)/man/man1
+MANDIR = $(PREFIX)/share/man/man1
 DOCDIR = $(PREFIX)/share/doc/ferm
-
-WEBDIR = /home/web/users/koka/ferm
-FTPDIR = /home/tecguest/ises/koka/anonftp
-
-# location of the old ferm script, for the test suite
-OLD_FERM = $(TOPDIR)/test/ferm1.1
-OLD_OPTIONS = --use iptables --lines --noexec --clearall --flushall --createchains
-
-NEW_FERM = $(TOPDIR)/src/ferm
-NEW_OPTIONS = --test
